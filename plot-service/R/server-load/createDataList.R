16c553cf1847213ffccec7cb8649a56e9158eacb
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2021-08-27 15:33:03 -0700

    fix: error parsing disk usage log

--- plot-service/R/server-load/createDataList.R
+++ plot-service/R/server-load/createDataList.R
@@ -7,25 +7,26 @@
 ########################################################################
 
 createDataList <- function(
-  infoList = NULL, 
+  infoList = NULL,
   dataDir = NULL
 ) {
-  
+
   logger.debug("----- server-load/createDataList() -----")
-  
+
   # ----- Validate parameters --------------------------------------------------
-  
+
   MazamaCoreUtils::stopIfNull(infoList)
-  
+
   # ----- Load uptime data ------------------------------------------------------------
-  
+
   # NOTE:  Need to watch out for reboots that change the number of commas
   #
   # 2018-06-07 18:16:01 up 35 days, 59 min,  0 users,  load average: 0.05, 0.01, 0.09
   # 2018-06-07 18:31:01 up 1 min,  0 users,  load average: 3.70, 1.99, 0.76
   #
   # Sigh ... Why is nothing ever easy?
-  
+
+  serverProtocol <- infoList$serverprotocol
   serverID <- infoList$serverid
   startDate <- MazamaCoreUtils::parseDatetime(infoList$startdate, timezone = "UTC")
 
@@ -36,63 +37,63 @@ createDataList <- function(
   }
 
   loadData = NULL
-  
+
   result <- try({
-    uptimeLogUrl <- paste0('https://', serverID, '/logs/uptime.log')
-    
+    uptimeLogUrl <- paste0(serverProtocol, '://', serverID, '/logs/uptime.log')
+
     # Instead, load the data as lines for further parsing
     lines <- readr::read_lines(uptimeLogUrl)
-    
+
     # Pull out elements using
-    regex_datetime <- "([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})"
+    regex_datetime <- "([0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2})" # Either " " or "T" between ymd and hms
     datetimeString <- stringr::str_extract(lines, regex_datetime)
-    
+
     regex_users <- "([0-9]+ user.?,)"
     usersString <- stringr::str_extract(lines, regex_users)
     # For userCount, use everything to the left of the first ' '
     usersString = stringr::str_split_fixed(usersString, ' ', 2)[,1]
-    
+
     regex_load <- "(load average: .+$)"
     loadString <- stringr::str_extract(lines, regex_load)
     loadString <- stringr::str_replace(loadString, "load average: ", "")
     loadString <- stringr::str_replace_all(loadString, " ", "")
-    
-    # Now reassemble a cleaned up, artificial CSV file 
+
+    # Now reassemble a cleaned up, artificial CSV file
     fakeLines <- paste(datetimeString, usersString, loadString, sep = ",")
     # Omit any lines with "NA"
     fakeLines <- fakeLines[ !stringr::str_detect(fakeLines, "NA") ]
     fakeFile <- paste(fakeLines, collapse = "\n")
-    
+
     loadData <- readr::read_csv(
       file = fakeFile,
       col_names = c('datetime', 'userCount', 'load_1_min', 'load_5_min', 'load_15_min'),
       col_types = "Tiddd"
     )
-    
+
     # Use dplyr to filter
     loadData <-
       loadData %>%
       filter(datetime >= startDate)
-    
+
   }, silent = TRUE)
-  
-  # Create dummy data to use if the uptime log is unavailible 
+
+  # Create dummy data to use if the uptime log is unavailible
   if ("try-error" %in% class(result)) {
     err_msg = geterrmessage()
     logger.trace(err_msg)
     loadData <- data.frame(Sys.time(), 0)
     colnames(loadData) <- c("datetime", "load_15_min")
   }
-  
+
   # ----- Load free memory data --------------------------------------------------
-  
+
   memoryData = NULL
-  
+
   result <- try({
-    memoryLogUrl <- paste0('https://', serverID, '/logs/free_memory.log')
+    memoryLogUrl <- paste0(serverProtocol, '://', serverID, '/logs/free_memory.log')
     col_names <- c('datetime','dummy','total','used','free','shared','buff_cache','available')
     memoryData <- readr::read_fwf(
-      file = memoryLogUrl, 
+      file = memoryLogUrl,
       col_positions = readr::fwf_empty(memoryLogUrl, col_names = col_names),
       col_types = "Tciiiiii"
     )
@@ -103,50 +104,68 @@ createDataList <- function(
       mutate(unavailable = total - available) %>%
       filter(datetime >= startDate)
   }, silent = TRUE)
-  
-  # Create dummy data to use if the memory log is unavailible 
+
+  # Create dummy data to use if the memory log is unavailible
   if ("try-error" %in% class(result)) {
     err_msg <- geterrmessage()
     logger.trace(err_msg)
     memoryData <- data.frame(Sys.time(), 0, 0, 0, 0, 0, 0, 0)
     colnames(memoryData) <- c('datetime','dummy','total','used','free','shared','buff_cache','available')
   }
-  
+
   # ----- Load disk usage data -------------------------------------------------
-  
+
+  # DO ubuntu
+  # 2021-07-16T22:00:01+00:00 /dev/vda1       25226960 7087848  18122728  29% /
+  # 2021-07-16T22:15:01+00:00 /dev/vda1       25226960 7516736  17693840  30% /
+  # 2021-07-16T22:30:01+00:00 /dev/vda1       25226960 9543096  15667480  38% /
+  # 2021-07-16T22:45:02+00:00 /dev/vda1       25226960 9733624  15476952  39% /
+
+  # tools-c1.airfire.org
+  # 2020-09-08T15:15:01+00:00 /dev/xvda1     130046416 58468340  71561692  45% /
+  # 2020-09-08T15:30:01+00:00 /dev/xvda1     130046416 57108020  72922012  44% /
+  # 2020-09-08T15:45:01+00:00 /dev/xvda1     130046416 57121196  72908836  44% /
+  # 2020-09-08T16:00:01+00:00 /dev/xvda1     130046416 57149248  72880784  44% /
+
+
   diskData  = NULL
-  
+
   result <- try({
-    diskLogUrl <- paste0('https://', serverID, '/logs/disk_usage.log')
-    col_names <- c('datetime','dummy1','dummy2','dummy3','dummy4','used','dummy5')
+    diskLogUrl <- paste0(serverProtocol, '://', serverID, '/logs/disk_usage.log')
+    col_names <- c('datetime','dummy1','dummy2','dummy3','dummy4','used')
     diskData <- readr::read_fwf(
       file = diskLogUrl,
       col_positions = readr::fwf_empty(diskLogUrl, col_names = col_names),
-      col_types = "Tciiicc"
+      col_types = "Tciiic"
     )
     diskData$dummy <- NULL
     diskData <-
       diskData %>%
       filter(datetime >= startDate)
-    diskData$used <- as.numeric(sub('%', '', diskData$used)) / 100
+    diskData$used <-
+      diskData$used %>%
+      stringr::str_replace_all("%", "") %>%
+      str_replace_all("/", "") %>%
+      as.numeric()
+    diskData$used <- diskData$used / 100
   }, silent = TRUE)
-  
-  # Create dummy data to use if the usage log is unavailible 
+
+  # Create dummy data to use if the usage log is unavailible
   if ("try-error" %in% class(result)) {
     err_msg <- geterrmessage()
     logger.trace(err_msg)
     diskData <- data.frame(Sys.time(), 0)
     colnames(memoryData) <- c("datetime", "used")
   }
-  
+
   # ----- Create data structures -----------------------------------------------
-  
+
   # Create dataList
   dataList <- list(
     loadData = loadData,
     memoryData = memoryData,
     diskData = diskData
   )
-  
+
   return(dataList)
 }
--- plot-service/R/server-load/createInfoList.R
+++ plot-service/R/server-load/createInfoList.R
@@ -14,73 +14,75 @@ createInfoList <- function(
   req = NULL,
   cacheDir = NULL
 ) {
-  
+
   logger.debug("----- server-load/createInfoList() -----")
-  
+
   # ----- Validate parameters --------------------------------------------------
-  
+
   MazamaCoreUtils::stopIfNull(req)
   MazamaCoreUtils::stopIfNull(cacheDir)
-  
+
   # Initialize the infoList from the request parameters
   infoList <- req$parameters
   names(infoList) <- tolower(names(infoList))
-  
+
   logger.debug("req$parameters")
   logger.debug(capture.output(str(req$parameters)))
-  
+
   # ----- Check for required parameters ----------------------------------------
-  
-  
+
+  MazamaCoreUtils::stopIfNull(infoList$serverid)
+
   # ----- Set parameter defaults -----------------------------------------------
-  
+
   # Set defaults
-  infoList$serverid <- tolower(ifelse(is.null(infoList$serverid), "joule.mazamascience.com", infoList$serverid))
+  infoList$serverprotocol <- tolower(ifelse(is.null(infoList$serverprotocol), "https", infoList$serverprotocol))
+  infoList$serverid <- tolower(infoList$serverid)
   # NOTE:  During plotting, ymax will take the maximum of the data maximum or infoList$ymax
   # NOTE:  We default to a small number so that data maximum will be used unless the users specifies something larger
   infoList$ymax <- ifelse(is.null(infoList$ymax), .01, as.numeric(infoList$ymax))
-  
+
   infoList$language <- tolower(ifelse(is.null(infoList$language),"en", infoList$language))
   infoList$responsetype <- tolower(ifelse(is.null(infoList$responsetype), "raw", infoList$responsetype))
   infoList$lookbackdays <- ifelse(is.null(infoList$lookbackdays), 3, trunc(as.numeric(infoList$lookbackdays)))
-  
+
   infoList$outputfiletype <- ifelse(is.null(infoList$outputfiletype), "png", infoList$outputfiletype)
-  
+
   infoList$width <- ifelse(is.null(infoList$width), 10, as.numeric(infoList$width))
   infoList$height <- ifelse(is.null(infoList$height), 6, as.numeric(infoList$height))
   infoList$units <- ifelse(is.null(infoList$units), "in", infoList$units)
   infoList$dpi <- ifelse(is.null(infoList$dpi), 100, as.numeric(infoList$dpi))
-  
+
   # Validate parameters
   if (!infoList$language %in% c("en","es")) { stop("invalid language", call. = FALSE) }
   if (!infoList$responsetype %in% c("raw", "json")) { stop("invalid responsetype", call. = FALSE) }
   if (!infoList$outputfiletype %in% c("png", "pdf")) { stop("invalid file format", call. = FALSE) }
   if (!infoList$units %in% c("in", "cm", "mm")) { stop("invalid units", call. = FALSE) }
   if (infoList$lookbackdays < 2 ) { infoList$lookbackdays <- 2 }
-  
+
   # NOTE:  enddate is specified here for creating the uniqueList. Enddate for plotting is specified
   # NOTE:  in the plotting function using localTime to set the default.
-  
+
   if ( is.null(infoList$enddate) ) {
     infoList$enddate <- strftime(lubridate::now(tzone = "UTC"), format = "%Y%m%d%H%M", tz = "UTC")
   }
-  
+
   if ( is.null(infoList$startdate) ) {
     endtime <- MazamaCoreUtils::parseDatetime(infoList$enddate, timezone = "UTC")
     starttime <- endtime - lubridate::ddays(infoList$lookbackdays)
     infoList$startdate <- strftime(starttime, format = "%Y%m%d%H%M", tz = "UTC")
   }
-  
+
   # TODO:  Could validate times and order at this point
-  
+
   # ----- Create uniqueID based on parameters that affect the presentation -----
-  
+
   # Update timestamp once every 5 minutes to guarantee plenty of cache hits
   timestamp <-
     lubridate::now(tzone = "UTC") %>%
     lubridate::floor_date("5 mins") %>%
     strftime("%Y%m%d%H%M")
-    
+
   # TODO: handle creating unique plots for shorter time intervals
   uniqueList <- list(
     infoList$language,
@@ -93,35 +95,35 @@ createInfoList <- function(
     infoList$lookbackdays,
     timestamp
   )
-  
+
   infoList$uniqueID <- digest::digest(uniqueList, algo = "md5")
-  
+
   # Create paths
   infoList$basePath <- paste0(cacheDir, "/", infoList$uniqueID)
   infoList$plotPath <- paste0(infoList$basePath, ".", infoList$outputfiletype)
   infoList$jsonPath <- paste0(infoList$basePath, ".json")
-  
+
   return(infoList)
-  
+
 }
 
 # ===== DEBUGGING ==============================================================
 
 if ( FALSE ) {
-  
+
   library(MazamaCoreUtils)
   logger.setup()
   logger.setLevel(TRACE)
-  
+
   req <- list(
     parameters = list(
       serverid = "joule.mazamascience.com",
       lookbackdays = "3"
     )
   )
-  
+
   cacheDir = "~/Projects/MazamaScience/mazama-plot-service/plot-service/output"
-  
+
 }
 
 
